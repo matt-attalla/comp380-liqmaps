85c2cbca13d093f909ea4afd5ac2df537ccd40d6
Matt Attalla <matt.attalla23@gmail.com>
2020-06-09 18:47:02 -0700

    Customized the footer.php file and added the bootstrap javascript needed.

---
+++ themes/alluring-ecommerce/footer.php
@@ -0,0 +1,46 @@
+</div><!-- #content -->
+</div>
+
+
+
+
+<footer class="container">
+
+    <div class="d-flex flex-wrap">
+        <div class="col-lg-4">
+            <h3>
+                Liqmaps
+            </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nam dignissimos adipisci consectetur enim quod voluptatem.</p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Menu</h3>
+        </div>
+        <div class="col-lg-4">
+            <h3>
+                Follow us
+            </h3>
+            <ul class="list-unstyled m-0 p-0">
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Twitter</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="site-info text-center">
+        <?php esc_html_e('Copyright', 'gutenshop'); ?> <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a><?php esc_html_e(' All rights reserved', 'gutenshop'); ?>
+        <!-- Delete below lines to remove copyright from footer -->
+
+    </div><!-- .site-info -->
+</footer>
+
+
+<?php wp_footer(); ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
