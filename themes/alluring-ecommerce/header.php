31f83d8a23b05b7e6ead888c50fc9c29c221c84b
Matt Attalla <matt.attalla23@gmail.com>
2020-06-09 19:26:09 -0700

    Added fontawesome 5 cdn for nice svg icons.

--- themes/alluring-ecommerce/header.php
+++ themes/alluring-ecommerce/header.php
@@ -5,7 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<?php wp_head(); ?>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
