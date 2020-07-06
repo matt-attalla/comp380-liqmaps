62592b7e8dcb90c207a1fb5d6285fef2b33bcfcd
Matt Attalla <matt.attalla23@gmail.com>
2020-07-06 12:05:10 -0700

    Added margin to the products to space them out from each other vertically.

--- themes/alluring-ecommerce/woocommerce/content-product.php
+++ themes/alluring-ecommerce/woocommerce/content-product.php
@@ -24,7 +24,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php wc_product_class( 'col-md-6 col-lg-3 text-center', $product ); ?>>
+<li <?php wc_product_class( 'col-md-6 col-lg-3 text-center my-4', $product ); ?>>
 	<?php
 	/**
 	 * Hook: woocommerce_before_shop_loop_item.
